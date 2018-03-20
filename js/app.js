8e29ebd127d582fd3e238c447ef7f2694ec75024
Orsolya Kanya <orsolya.kanya@yahoo.com>
2018-03-20 18:26:21 -0400

    adding font

--- js/app.js
+++ js/app.js
@@ -1,15 +1,186 @@
 const clickedImage1 = document.querySelector('#pic1');
 const cardName1 = document.querySelector('#cardtitle1');
 const linkList1 = document.querySelector('#overlay1');
+const clickedImage2 = document.querySelector('#pic2');
+const cardName2 = document.querySelector('#cardtitle2');
+const linkList2 = document.querySelector('#overlay2');
+const clickedImage3 = document.querySelector('#pic3');
+const cardName3 = document.querySelector('#cardtitle3');
+const linkList3 = document.querySelector('#overlay3');
+const clickedImage4 = document.querySelector('#pic4');
+const cardName4 = document.querySelector('#cardtitle4');
+const linkList4 = document.querySelector('#overlay4');
+const clickedImage5 = document.querySelector('#pic5');
+const cardName5 = document.querySelector('#cardtitle5');
+const linkList5 = document.querySelector('#overlay5');
+const clickedImage6 = document.querySelector('#pic6');
+const cardName6 = document.querySelector('#cardtitle6');
+const linkList6 = document.querySelector('#overlay6');
+
+
+
+
 
 clickedImage1.addEventListener('click', () => {
   cardName1.style.display = "none";
   linkList1.style.display = "block";
+  }
+);
+
+
+clickedImage2.addEventListener('click', () => {
+  cardName2.style.display = "none";
+  linkList2.style.display = "block";
 }
 );
 
-linkList1.addEventListener('mouseout', () => {
+  clickedImage3.addEventListener('click', () => {
+  cardName3.style.display = "none";
+  linkList3.style.display = "block";
+}
+);
+
+clickedImage4.addEventListener('click', () => {
+  cardName4.style.display = "none";
+  linkList4.style.display = "block";
+}
+);
+
+
+clickedImage5.addEventListener('click', () => {
+  cardName5.style.display = "none";
+  linkList5.style.display = "block";
+}
+);
+
+
+clickedImage6.addEventListener('click', () => {
+  cardName6.style.display = "none";
+  linkList6.style.display = "block";
+}
+);
+
+clickedImage1.addEventListener('mouseover', () => {
+  cardName2.style.display = "block";
+  linkList2.style.display = "none";
+  cardName3.style.display = "block";
+  linkList3.style.display = "none";
+  cardName4.style.display = "block";
+  linkList4.style.display = "none";
+  cardName5.style.display = "block";
+  linkList5.style.display = "none";
+  cardName6.style.display = "block";
+  linkList6.style.display = "none";
+}
+);
+
+clickedImage2.addEventListener('mouseover', () => {
   cardName1.style.display = "block";
   linkList1.style.display = "none";
+  cardName3.style.display = "block";
+  linkList3.style.display = "none";
+  cardName4.style.display = "block";
+  linkList4.style.display = "none";
+  cardName5.style.display = "block";
+  linkList5.style.display = "none";
+  cardName6.style.display = "block";
+  linkList6.style.display = "none";
+}
+);
+
+clickedImage3.addEventListener('mouseover', () => {
+  cardName1.style.display = "block";
+  linkList1.style.display = "none";
+  cardName2.style.display = "block";
+  linkList2.style.display = "none";
+  cardName4.style.display = "block";
+  linkList4.style.display = "none";
+  cardName5.style.display = "block";
+  linkList5.style.display = "none";
+  cardName6.style.display = "block";
+  linkList6.style.display = "none";
+}
+);
+
+clickedImage4.addEventListener('mouseover', () => {
+  cardName1.style.display = "block";
+  linkList1.style.display = "none";
+  cardName2.style.display = "block";
+  linkList2.style.display = "none";
+  cardName3.style.display = "block";
+  linkList3.style.display = "none";
+  cardName5.style.display = "block";
+  linkList5.style.display = "none";
+  cardName6.style.display = "block";
+  linkList6.style.display = "none";
+}
+);
+
+clickedImage5.addEventListener('mouseover', () => {
+  cardName1.style.display = "block";
+  linkList1.style.display = "none";
+  cardName2.style.display = "block";
+  linkList2.style.display = "none";
+  cardName3.style.display = "block";
+  linkList3.style.display = "none";
+  cardName4.style.display = "block";
+  linkList4.style.display = "none";
+  cardName6.style.display = "block";
+  linkList6.style.display = "none";
+}
+);
+
+clickedImage6.addEventListener('mouseover', () => {
+  cardName1.style.display = "block";
+  linkList1.style.display = "none";
+  cardName2.style.display = "block";
+  linkList2.style.display = "none";
+  cardName3.style.display = "block";
+  linkList3.style.display = "none";
+  cardName4.style.display = "block";
+  linkList4.style.display = "none";
+  cardName5.style.display = "block";
+  linkList5.style.display = "none";
+}
+);
+
+
+
+clickedImage1.addEventListener('click', () => {
+  cardName1.style.display = "none";
+  linkList1.style.display = "block";
+  }
+);
+
+
+clickedImage2.addEventListener('click', () => {
+  cardName2.style.display = "none";
+  linkList2.style.display = "block";
+}
+);
+
+  clickedImage3.addEventListener('click', () => {
+  cardName3.style.display = "none";
+  linkList3.style.display = "block";
+}
+);
+
+clickedImage4.addEventListener('click', () => {
+  cardName4.style.display = "none";
+  linkList4.style.display = "block";
+}
+);
+
+
+clickedImage5.addEventListener('click', () => {
+  cardName5.style.display = "none";
+  linkList5.style.display = "block";
+}
+);
+
+
+clickedImage6.addEventListener('click', () => {
+  cardName6.style.display = "none";
+  linkList6.style.display = "block";
 }
 );
